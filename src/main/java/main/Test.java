3d97ad43f674bc0030154017c7559622518b4b33
oshnix <sdsdaniil@gmail.com>
2017-10-28 11:06:21 +0300

    Test insert added

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import service.coords.Coords;
 import service.galaxies.Galaxies;
 import service.galaxies.Galaxies_DAO;
+import service.objectCommon.ObjectCommon;
 import service.stars.Stars_DAO;
 
 import java.util.List;
@@ -13,6 +14,9 @@ import java.util.Random;
  * Created by 12 on 28.10.2017.
  */
 public class Test {
+
+    private static Galaxies_DAO galaxyDao;
+
     private static Galaxies randomGalaxy(Integer id){
         Random r=new Random();
         Galaxies g=new Galaxies();
@@ -20,14 +24,34 @@ public class Test {
         g.setGalaxy_coordinates(new Coords(r.nextFloat(),r.nextFloat()));
         return g;
     }
+
+    private static boolean testInsert(String name, Integer objectType, Coords galaxyCoords){
+        ObjectCommon objectCommon = new ObjectCommon(name, objectType);
+        Galaxies galaxy = new Galaxies(galaxyCoords);
+        galaxyDao.save(objectCommon, galaxy);
+
+        Galaxies gal = new Galaxies();
+        gal.setObject_id(galaxy.getObject_id());
+        gal = galaxyDao.read(gal);
+
+        return galaxy.getObject_id() != null && gal.getCoords().equals(galaxy.getCoords());
+    }
+
+
     public static boolean testUpdate(Integer id){
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        Galaxies_DAO galaxyDao=context.getBean(Galaxies_DAO.class);
         Galaxies g=randomGalaxy(id);
         Galaxies oldG=g;
         galaxyDao.update(g);
         galaxyDao.read(oldG);
-        context.close();
         return g.getCoords()==oldG.getCoords()&&g.getObject_id()==g.getObject_id();
+    }
+
+    public static boolean test(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        galaxyDao = context.getBean(Galaxies_DAO.class);
+        //вызов функций
+
+        context.close();
+        return true;
     }
 }
